e5d4f2b717f4ae8382a0d4a0fa2da6a56f7c2693
Triologee <64104688+Triologee@users.noreply.github.com>
2021-01-22 03:16:18 +0800

    Timepicker fix
    Fixed Timepicker, added edit event activity (blank for now)
    
    TODO:
    1. Location tagging
    2. Edit event
    3. "Edit event" button at event details

--- app/src/main/java/com/example/volunteeringapp/ManageEventsActivity.java
+++ app/src/main/java/com/example/volunteeringapp/ManageEventsActivity.java
@@ -3,26 +3,46 @@ package com.example.volunteeringapp;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class ManageEventsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ManageEventsActivity extends AppCompatActivity {
+    final Fragment eventsF = new fragment_manage_events();
     DBHelper DB;
-    Button btn_test;
+    Button btn_test, btn_test2;
+    private String mParam1;
+    private String mParam2;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter eventAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    List<Event> eventList;
+    Fragment active = eventsF;
+    final FragmentManager fm = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_events);
 
+        fm.beginTransaction().add(R.id.frameFragment, eventsF, "1").commit();
+
         DB = new DBHelper(this);
         btn_test = findViewById(R.id.btn_test);
+        btn_test2 = findViewById(R.id.btn_test2);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -56,8 +76,19 @@ public class ManageEventsActivity extends AppCompatActivity {
             }
         });
 
+        btn_test2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(ManageEventsActivity.this, "Test", Toast.LENGTH_SHORT).show();
 
+                Intent editEvent = new Intent (ManageEventsActivity.this, EditEventActivity.class);
+                editEvent.putExtra("event_id", "1");
+                startActivityForResult(editEvent, 1);
+            }
+        });
     }
+
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
---
+++ app/src/main/java/com/example/volunteeringapp/fragment_manage_events.java
@@ -0,0 +1,104 @@
+package com.example.volunteeringapp;
+
+import android.database.Cursor;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link fragment_manage_events#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fragment_manage_events extends Fragment {
+
+    DBHelper DB;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter eventAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    List<Event> eventList;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public fragment_manage_events() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment fragment_manage_events.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static fragment_manage_events newInstance(String param1, String param2) {
+        fragment_manage_events fragment = new fragment_manage_events();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_manage_events, container, false);
+        recyclerView =  view.findViewById(R.id.rv_events);
+        eventList = new ArrayList<Event>();
+        eventList.clear();
+        DB = new DBHelper(getContext());
+        Cursor res = DB.getAllEvents();
+        if(res != null && res.getCount()>0){
+            eventList.clear();
+            while (res.moveToNext()){
+                Event eventItem = new Event();
+                eventItem.setId(res.getInt(res.getColumnIndex("ID")));
+                eventItem.setEventTitle(res.getString(res.getColumnIndex("event_title")));
+                eventItem.setDescription(res.getString(res.getColumnIndex("description")));
+                eventItem.setCapacity(res.getInt(res.getColumnIndex("capacity")));
+                eventItem.setDate(res.getString(res.getColumnIndex("start_date")));
+                eventItem.setStartTime(res.getString(res.getColumnIndex("start_time")));
+                eventItem.setEndTime(res.getString(res.getColumnIndex("end_time")));
+                eventItem.setLocation(res.getString(res.getColumnIndex("location")));
+                eventItem.setOrganizerId(res.getString(res.getColumnIndex("organizer")));
+                eventList.add(eventItem);
+            }
+        }
+        res.close();
+        layoutManager = new LinearLayoutManager(getContext());
+        eventAdapter = new EventDetailsAdapter(eventList);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(eventAdapter);
+        return view;
+    }
+}
