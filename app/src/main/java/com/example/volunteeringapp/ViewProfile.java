a8769db1c6aeb7cb6f75c7a9c522e23eed86039b
cleopatez <46080143+cleopatez@users.noreply.github.com>
2021-01-22 06:40:45 +0800

    Added Follow and Rating logic.
    Logic for follow and rating modules added. Waiting for UI part to test the code.

--- app/src/main/java/com/example/volunteeringapp/ViewProfile.java
+++ app/src/main/java/com/example/volunteeringapp/ViewProfile.java
@@ -1,13 +1,17 @@
 package com.example.volunteeringapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class ViewProfile extends AppCompatActivity {
 
@@ -15,6 +19,7 @@ public class ViewProfile extends AppCompatActivity {
     Button btn_follow;
     Button btn_contact;
     Button btn_give_rating;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +31,85 @@ public class ViewProfile extends AppCompatActivity {
         btn_contact = (Button) findViewById(R.id.btn_contact);
         btn_give_rating = (Button) findViewById(R.id.btn_give_rating);
 
-        float currentAvgRating = 5;
-        ratingBar.setRating(5);
+        SessionManagement sessionManagement = new SessionManagement(ViewProfile.this);
+        String current_user = Integer.toString(sessionManagement.getSession());
+        int userID = sessionManagement.getSession();
+
+        Integer user_id = Integer.valueOf(current_user); // Retrieve the current user id
+        Integer follower_id = 2; // Retrieve the selected user's id
+
+        DB = new DBHelper(this);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Cursor GetFollowing = DB.getFollowing(userID);
+                Cursor GetFollowers = DB.getFollowers(userID);
+
+                TextView following = (TextView) findViewById(R.id.ET_following_numbers);
+                TextView followers = (TextView) findViewById(R.id.ET_followers_numbers);
+
+                while(GetFollowers.moveToNext() && GetFollowing.moveToNext()){
+                    following.setText(GetFollowing.getString(0));
+                    followers.setText(GetFollowers.getString(1));
+                }
+            }
+        });
 
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
 
+                Cursor GetRating = DB.getAvgRating(userID);
+                Cursor GetRaters = DB.getRaters(userID);
 
+                //DISPLAY CURRENT RATING
+
+                float currentAvgRating = GetRating.getFloat(5);
+//                float currentAvgRating = 1;
+                Toast.makeText(ViewProfile.this, "Rating: " + String.valueOf(currentAvgRating), Toast.LENGTH_SHORT).show();
+
+                while(GetRating.moveToNext()){
+                    ratingBar.setRating(GetRating.getFloat(5));
+                }
+
+                //UPDATE CURRENT RATING
+                float numberOfRater = GetRaters.getCount();
                 float currentRating = ratingBar.getRating();
-                float calculatedAvgRating = (currentAvgRating + currentRating)/2;
+//                float currentRating = 1;
+                float calculatedAvgRating = (currentAvgRating + currentRating)/numberOfRater;
+//                float calculatedAvgRating = 2;
 
-                Toast.makeText(ViewProfile.this, "Rating: " + String.valueOf(calculatedAvgRating), Toast.LENGTH_SHORT).show();
-                ratingBar.setRating(calculatedAvgRating);
+                if (user_id.equals(follower_id)){
+                    Toast.makeText(ViewProfile.this, "Cannot rate yourself!", Toast.LENGTH_SHORT).show();
+                } else {
+                    DB.giveRating(user_id, follower_id, currentRating);
+                    DB.updateAvgRating(calculatedAvgRating, user_id);
+                    Toast.makeText(ViewProfile.this, "Rating: " + String.valueOf(calculatedAvgRating), Toast.LENGTH_SHORT).show();
+
+                }
+
+                while(GetRating.moveToNext()){
+                    ratingBar.setRating(GetRating.getFloat(2));
+                }
             }
         });
 
         btn_follow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ViewProfile.this, "Followed!", Toast.LENGTH_SHORT).show();
+
+                if(user_id.equals(follower_id)){
+                    Toast.makeText(ViewProfile.this, "Cannot follow yourself!", Toast.LENGTH_SHORT).show();
+                } else {
+                    DB.followUser(user_id, follower_id);
+                    Toast.makeText(ViewProfile.this, "Followed!", Toast.LENGTH_SHORT).show();
+                    Cursor res = DB.getFollowers(userID);
+                    TextView tv = (TextView) findViewById(R.id.ET_followers_numbers);
+                    while(res.moveToNext()){
+                        tv.setText(res.getString(1));
+                    }
+                }
             }
         });
 
