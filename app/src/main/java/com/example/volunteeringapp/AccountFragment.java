715698805c171e3fdcf939a3a651d6156898214e
Tofu Fa <iamifajoe@gmail.com>
2021-01-24 05:51:36 +0800

    rewards page

--- app/src/main/java/com/example/volunteeringapp/AccountFragment.java
+++ app/src/main/java/com/example/volunteeringapp/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.volunteeringapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,18 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        Button btn_Rewards = (Button) view.findViewById(R.id.btn_Rewards);
+        btn_Rewards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), activity_rewards_page.class);
+                intent.putExtra("some", "somela");
+                startActivity(intent);
+
+            }
+        });
+        return view;
     }
 }
