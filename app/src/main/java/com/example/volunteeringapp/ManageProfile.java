5edbd029642f8d69b3f8f108d42414f05f164543
Tofu Fa <iamifajoe@gmail.com>
2021-01-24 19:27:55 +0800

    manage profile

--- app/src/main/java/com/example/volunteeringapp/ManageProfile.java
+++ app/src/main/java/com/example/volunteeringapp/ManageProfile.java
@@ -1,11 +1,15 @@
 package com.example.volunteeringapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +18,8 @@ import androidx.appcompat.widget.Toolbar;
 
 public class ManageProfile extends AppCompatActivity {
 
-    private Button changepassword, savechange, discardchange;
-    private EditText name, email;
+    Button changepassword, savechange, discardchange;
+    EditText name, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,74 +28,12 @@ public class ManageProfile extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
 
-        EditText name = findViewById(R.id.ET_name);
-        EditText email = findViewById(R.id.ET_email);
+        name = findViewById(R.id.ET_name);
+        email = findViewById(R.id.ET_email);
 
-        Button savechange = findViewById(R.id.BTN_saveProfile);
-        Button discardchange = findViewById(R.id.BTN_discardProfileChange);
-
-        savechange.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                String nameinput = name.getText().toString();
-                name.setText("Saved");
-            }
-        });
+        //Button savechange = findViewById(R.id.BTN_saveProfile);
+        // Button discardchange = findViewById(R.id.BTN_discardProfileChange);
 
     }
-    public void onBackPressed(){
-        super.onBackPressed();
-    }
+
 }
-
-        /*private TextWatcher profileTextWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                String nameInput = name.getText().toString().trim();
-                String emailInput = email.getText().toString().trim();
-
-                savechange.setEnabled(!nameInput.isEmpty() && !emailInput.isEmpty());
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        };
-   // public void onClick(View v){
-     //   finish();
-    //}
-
-
-        savechange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((EditText) ManageProfile.this.findViewById(R.id.ET_name)).setText(name.getText().toString());
-                ((EditText) ManageProfile.this.findViewById(R.id.ET_email)).setText(email.getText().toString());
-            }
-        });
-
-        String nametext = name.getText().toString();
-        String emailtext = email.getText().toString();
-
-        if(nametext.length() == 0){
-            Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show();
-        }
-        else if (emailtext.length() == 0){
-            Toast.makeText(this, "Please enter your email", Toast.LENGTH_LONG).show();
-        }
-
-        else {
-            Toast.makeText(this, "Changes Saved", Toast.LENGTH_SHORT).show();
-        }
-
-    }*/
-
